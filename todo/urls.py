c0d9a49ef6ab11b47b6d6191d2906c3242a9b2c1
Your Name <you@example.com>
2021-08-21 20:33:51 +0900

    first commit todoapp
    基本

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Todolist, Tododetail, Todocreate, Tododelete, Todoupdate
+
+urlpatterns = [
+    path('', Todolist.as_view(), name='list'),
+    path('detail/<int:pk>', Tododetail.as_view(), name='detail'),
+    path('create/', Todocreate.as_view(), name='create'),
+    path('delete/<int:pk>', Tododelete.as_view(), name='delete'),
+    path('update/<int:pk>', Todoupdate.as_view(), name='update'),
+]
